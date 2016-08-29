b80731079b194bb0aa79014a03fc660b3c0d3caa
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-08-29 20:22:07 +0200

    Autoredirect the output

--- genCmdsForQS.sh
+++ genCmdsForQS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sql wikidatawiki < sql | sed 1d | sed "s/^/echo -e \'/g" | sed "s/$/\'/g" | bash
+sql wikidatawiki < sql | sed 1d | sed "s/^/echo -e \'/g" | sed "s/$/\'/g" | bash > cmds.txt
